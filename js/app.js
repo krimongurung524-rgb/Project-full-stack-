0f6f80e74d4c2c01e77ecd55e62b7357a4583d0f
Krimon Gurung <krimongurung524@gmail.com>
2026-07-30 22:11:21 +0545

    few codes are added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const orderBtn = document.getElementById("orderBtn");
+const loginBtn = document.getElementById("loginBtn");
+
+orderBtn.addEventListener("click", () => {
+    alert("Welcome to Smart Cafe ☕");
+});
+
+loginBtn.addEventListener("click", () => {
+    alert("Login page coming soon...");
+});
